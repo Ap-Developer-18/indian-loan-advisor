71faf8884f33a0da90e49575268e673dfb7c753e
Aman Punia <puniaaman369@gmail.com>
2026-07-30 10:27:51 +0530

    work on global css

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -197,7 +197,7 @@ export default function Footer() {
             </div>
             <div className="flex flex-col items-center gap-3 pt-2 w-full">
               <div className="w-10 h-10 rounded-xl bg-brand-muted border border-brand/20 flex items-center justify-center text-brand shrink-0">
-                <svg className="w-4 h-4 fill-current" viewBox="0 0 24 24">
+                <svg className="w-4 h-4 fill-[#87cefa]" viewBox="0 0 24 24">
                   <path d="M6.62 10.79a15.15 15.15 0 006.59 6.59l2.2-2.2c.28-.28.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z" />
                 </svg>
               </div>
